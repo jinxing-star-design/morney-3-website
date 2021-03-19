4ca183b634e24f6af17aa0a198cc280d599ebbe0
jinxing-star <jinxing97@126.com>
2021-03-19 19:06:18 +0800

    deploy

---
+++ precache-manifest.ca6702fe7cc98e268e119a07f49d3278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1928da7caf65e3fe0c",
+    "url": "/morney-3-website/css/app.8bd9a1d5.css"
+  },
+  {
+    "revision": "31c37a8ff45a7810c4a1ad3f9b02b856",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "5d1928da7caf65e3fe0c",
+    "url": "/morney-3-website/js/app.f05388c8.js"
+  },
+  {
+    "revision": "d8330e90997f127ab200",
+    "url": "/morney-3-website/js/chunk-vendors.e7a7b358.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
